8873d4af6866dd141b493e379fe74640f24b2bf3
Ian Cargill <idcargill@gmail.com>
2021-09-22 19:47:05 -0700

    add Like-O-meter

--- src/components/HornedBeasts.js
+++ src/components/HornedBeasts.js
@@ -1,16 +1,38 @@
 import React from 'react';
 
 class HornedBeasts extends React.Component {
-    render() {
-        return (
-            <>
-                <h2>{this.props.title}</h2>
-                <img src={this.props.src} alt='Beastly Pic' title={this.props.title}/>
-                <p>{this.props.description}</p>
-            </>
-        )
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      favorite: 0,
+    };
+  }
+
+  HandleClick = () => {
+    this.setState({ favorite: this.state.favorite + 1 });
+    console.log(this.state.favorite);
+  };
+
+  render() {
+    return (
+      <>
+        <h2>{this.props.title}</h2>
+        <img
+          src={this.props.image}
+          alt='Beastly Pic'
+          title={this.props.title}
+          onClick={this.HandleClick}
+        />
+        <p>Like-O-meter &#10084; {this.state.favorite}</p>
+        <p>{this.props.description}</p>
+      </>
+    );
+  }
 }
 
-
 export default HornedBeasts;
+
+/*
+&#10084;
+&#x2764;
+*/
